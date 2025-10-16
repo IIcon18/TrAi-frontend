882a061e968c4f965bf7fe142004ae01a5343e95
Ilya <145450507+IIcon18@users.noreply.github.com>
2025-10-16 14:06:26 +0300

    Настройка роутов, завершение формы регистрации.

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,8 +2,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 // import Login from "../pages/Login";
-import Register from "../pages/Register";
+import Register from "../components/pages/Register";
 // import Dashboard from "../pages/Dashboard";
+// import Profile from "../pages/Profile";
+// import Workouts from "../pages/Workouts";
+// import Progress from "../pages/Progress";
+// import NotFound from "../components/pages/NotFound";
 
 const AppRouter: React.FC = () => {
     return (
@@ -15,7 +19,11 @@ const AppRouter: React.FC = () => {
                 <Route path="/register" element={<Register />} />
 
                 {/*<Route path="/dashboard" element={<Dashboard />} />*/}
+                {/*<Route path="/profile" element={<Profile />} />*/}
+                {/*<Route path="/workouts" element={<Workouts />} />*/}
+                {/*<Route path="/progress" element={<Progress />} />*/}
 
+                {/* <Route path="*" element={<NotFound />} /> */}
             </Routes>
         </BrowserRouter>
     );
