50660c0a5793710ca683c607c8173e02cdb20648
Ilya <145450507+IIcon18@users.noreply.github.com>
2026-02-25 20:22:11 +0300

    fixes

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,4 +1,3 @@
-// src/router/AppRouter.tsx
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Register from "../components/pages/Register";
